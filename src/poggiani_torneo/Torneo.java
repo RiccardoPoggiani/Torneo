345e624d0b20cfcd744f193b31d54d2bde9ef860
RiccardoPoggiani <rikypoggiani06@gmail.com>
2024-12-10 10:32:32 +0100

    Modifica Torneo.java

--- src/poggiani_torneo/Torneo.java
+++ src/poggiani_torneo/Torneo.java
@@ -1,18 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * @author Riccardo Poggiani
+ * @see Scheda esercitazione
  */
 package poggiani_torneo;
 
-/**
- *
- * @author 1
- */
 public class Torneo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Test Main");
         System.out.println("Classe Arbitro creata");
